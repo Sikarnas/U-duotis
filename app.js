7d184673774633473280533f901e0f8be7c90246
Aurelijus <auritoz95@gmail.com>
2019-08-23 13:26:22 +0300

    formated code properly

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const fs = require('fs')
-const calculateCommissions = require('./utils/calculateCommissions')
+const fs = require("fs");
+const calculateCommissions = require("./utils/calculateCommissions");
 
-const fileName = process.argv[2]
-const operations = JSON.parse(fs.readFileSync(fileName))
+const fileName = process.argv[2];
+const operations = JSON.parse(fs.readFileSync(fileName));
 
 const parameters = {
-    weeklyLimit: 0, 
-    cashInCommission: 0.0003, 
-    naturalCommissions: 0.003, 
-    legalCommissions: 0.003, 
-    maxCashInFee: 5, 
-    minLegalCashOutFee: 0.5
-}
+  weeklyLimit: 0,
+  cashInCommission: 0.0003,
+  naturalCommissions: 0.003,
+  legalCommissions: 0.003,
+  maxCashInFee: 5,
+  minLegalCashOutFee: 0.5
+};
 
 // prints fees to the console, you can pass object as a 3rd parameter with different values or it will use default values
 
-calculateCommissions(operations, (fee) => console.log(fee)/*, parameters*/)
+calculateCommissions(operations, fee => console.log(fee) /*, parameters*/);
